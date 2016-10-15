33d781a849ac4462a73030221f2b57ba4ed71538
Dck Token <dicktoken@gmail.com>
2016-10-15 19:44:37 +0200

    Fixed #1 (updated to 3.6)

--- sodoku-solver.py
+++ sodoku-solver.py
@@ -1,6 +1,10 @@
 def printBoard(board):
-	print(" ╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗ ").encode('utf-8')
+	print(" ╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗ ")
 	for i in ["a" , "b", "c", "d", "e", "f", "g", "h", "i"]:
-		print(i + "║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║")		
+		print(i + "║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║ " + board[0][0] + " │ " + board[0][0] + " │ " + board[0][0] + " ║")
+		if i is not "i":
+			print(" ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢")
+	print(" ╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝ ")
+	print("   1   2   3   4   5   6   7   8   9 ")
 board = [[" "]]
 printBoard(board)
